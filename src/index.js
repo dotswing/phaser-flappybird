823c559985bcc537770f27542cf1c81cf02848dd
Arut Thanomwatana <arut.tha@gmail.com>
2019-07-05 15:05:29 +0700

    Add random pipe

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
 import sky from "./assets/sky.png"
 import land from "./assets/land.png"
+import pipe from "./assets/pipe.png"
 import bird from "./assets/bird.png";
 
 var config = {
@@ -11,7 +12,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 0 },
             debug: false
         }
     },
@@ -22,27 +23,57 @@ var config = {
     }
 };
 
-var platforms;
+var lowerPipes;
+var upperPipes;
 
 var landImg;
 var iter = 0;
 const game = new Phaser.Game(config);
+var keyboards
+var player
+var emptySpace
 
 function preload() {
   this.load.image('sky', sky);
   this.load.image("logo", logoImg);
   this.load.image("land", land);
+  this.load.image("pipe", pipe)
   this.load.spritesheet("bird", bird, { frameWidth: 34, frameHeight: 24 });
 }
 
 function create() {
+  keyboards = this.input.keyboard.createCursorKeys()
   this.add.image(400, 300, 'sky');
-  const bird = this.physics.add.sprite(240, 320, 'bird', 0);
+  player = this.physics.add.sprite(240, 320, 'bird', 0);
+  player.setGravityY(300)
+  upperPipes = this.physics.add.group({
+    key: 'pipe',
+    repeat: 5,
+    setXY: { x: 400, y: 0, stepX: 200 },
+    setScale: {x : 1, y: -100}
+  })
 
-  platforms = this.physics.add.staticGroup();
+  lowerPipes = this.physics.add.group({
+    key: 'pipe',
+    repeat: 5,
+    setXY: { x: 400, y: 500, stepX: 200 },
+    setScale: {x : 1, y: 100}
+  });
 
+  emptySpace = this.physics.add
+
+  lowerPipes.children.iterate(function (child) {
+    child.setVelocityX(-60.6)
+    console.log(child)
+  })
+  upperPipes.children.iterate(function (child) {
+    child.setVelocityX(-60.6)
+  })
+
+  // pipes.create(400, 500, 'pipe').setScale(1, 100).refreshBody()
+  // pipes.setVelocityX(-10)
   // 112 Is land image height
-  landImg = this.add.tileSprite(400, 600 - (112/2), 800, 112, 'land');
+  landImg = this.add.tileSprite(400, 600 - (50), 800, 100, 'land');
 
   this.anims.create({
     key: 'fly',
@@ -50,10 +81,48 @@ function create() {
     repeat: -1,
   });
 
-  bird.anims.play('fly');
+  player.anims.play('fly');
+
+  // this.physics.add.overlap(player, landImg, dead, null, this)
+  this.physics.add.overlap(player, lowerPipes, dead, null, this)
+  
 }
 
 function update() {
   landImg.tilePositionX = iter * 100;
   iter += 0.01;
+  if (keyboards.space.isDown) {
+    player.setVelocityY(-100)
+  }
+
+  lowerPipes.children.iterate(function (child) {
+    let posX = child.x
+    if (posX < -50) {
+      child.x = 850
+      child.scaleY = randomLowerPipe()
+    }
+  })
+  upperPipes.children.iterate(function (child) {
+    let posX = child.x
+    if (posX < -50) {
+      child.x = 850
+      child.scaleY = randomUpperPipe()
+    }
+  })
+}
+
+function dead(player, other) {
+  console.log('You dead')
+}
+
+function randomChong() {
+  return Math.floor(Math.random() * 400) + 100
+}
+
+function randomLowerPipe() {
+  return Math.floor(Math.random() * 125) + 100
+}
+
+function randomUpperPipe() {
+  return (Math.floor(Math.random() * 125) + 100) * -1
 }
