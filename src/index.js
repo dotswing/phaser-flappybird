f3aa1bd9555683c7320f173d83e3174c397dea3e
Tanarat <tanarat.n@linecorp.com>
2019-07-05 13:16:11 +0700

    Add bird

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
 import sky from "./assets/sky.png"
 import land from "./assets/land.png"
+import bird from "./assets/bird.png";
 
 var config = {
     type: Phaser.AUTO,
@@ -31,16 +32,25 @@ function preload() {
   this.load.image('sky', sky);
   this.load.image("logo", logoImg);
   this.load.image("land", land);
+  this.load.spritesheet("bird", bird, { frameWidth: 34, frameHeight: 24 });
 }
 
 function create() {
   this.add.image(400, 300, 'sky');
-
+  const bird = this.physics.add.sprite(240, 320, 'bird', 0);
 
   platforms = this.physics.add.staticGroup();
 
   // 112 Is land image height
   landImg = this.add.tileSprite(400, 600 - (112/2), 800, 112, 'land');
+
+  this.anims.create({
+    key: 'fly',
+    frames: this.anims.generateFrameNames('bird', { start: 0, end: 3 }),
+    repeat: -1,
+  });
+
+  bird.anims.play('fly');
 }
 
 function update() {
